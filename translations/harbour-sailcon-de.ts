9f0e8daba5911e538b384f4bb59def29cfedd862
andywuest <andreas.wuest.freelancer@gmail.com>
2019-03-28 22:25:19 +0100

    initial import

---
+++ translations/harbour-sailcon-de.ts
@@ -0,0 +1,212 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="about-title">
+        <source>About SailCon</source>
+        <extracomment>About page title, label and menu item</extracomment>
+        <translation>Über SailCon</translation>
+    </message>
+    <message id="about-description">
+        <source>This app demonstrates a bar code reader for Sailfish OS. I hope it is useful for other projects. CodeReader is open source and licensed under the MIT License.</source>
+        <extracomment>About page text</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="about-version-label">
+        <source>Version</source>
+        <extracomment>About page label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="about-author-label">
+        <source>Author</source>
+        <extracomment>About page label</extracomment>
+        <translation>Autor</translation>
+    </message>
+    <message id="about-source_code-label">
+        <source>Source code</source>
+        <extracomment>About page label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExplorationTypeSelection</name>
+    <message>
+        <source>Exploration Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExploreByAudience</name>
+    <message>
+        <source>Time Selection</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExploreByDay</name>
+    <message>
+        <source>Event Day</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExploreByDayAndTime</name>
+    <message>
+        <source>Day : </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExploreByLocation</name>
+    <message>
+        <source>Location Selection</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExploreByTime</name>
+    <message>
+        <source>Time Selection</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExploreByTrack</name>
+    <message>
+        <source>Track Selection</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>DukeCon App</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select Conference</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Conferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>active conference</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>available conferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>please select conference</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished">About YYYY</translation>
+    </message>
+</context>
+<context>
+    <name>LoadingIndicator</name>
+    <message>
+        <source>Loading...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Nested Page</source>
+        <translation>Unterseite</translation>
+    </message>
+    <message>
+        <source>Item</source>
+        <translation>Element</translation>
+    </message>
+</context>
+<context>
+    <name>SelectConference</name>
+    <message>
+        <source>Available Conferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Conference data unchanged.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Talk</name>
+    <message>
+        <source>Selected Talk</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TalkOld</name>
+    <message>
+        <source>Selected Talk</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TalkSettings</name>
+    <message>
+        <source>Talk Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Talks</name>
+    <message>
+        <source>Talks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Uhr</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>database</name>
+    <message>
+        <source>Conference data updated.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Conference data stored.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to store conference data.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Conference images updated.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to store conference images.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
