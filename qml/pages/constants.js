66c36d5a6048ae2cc39391be33108eb795a71857
andywuest <andreas.wuest.freelancer@gmail.com>
2019-04-25 23:27:48 +0200

    added binary download service

--- qml/pages/constants.js
+++ qml/pages/constants.js
@@ -20,3 +20,22 @@ var VERSION = "0.1"
 // you need to add you local ip address to make sure the vb image can access the server
 var CONFERENCES_URL = 'http://192.168.123.128:8083/rest/conferences';
 var SINGLE = true;
+
+//FROM https://cdnjs.cloudflare.com/ajax/libs/Base64/1.0.1/base64.js
+function base64Encode (input) {
+    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+    var str = String(input);
+    for (
+        // initialize result and counter
+        var block, charCode, idx = 0, map = chars, output = '';
+        str.charAt(idx | 0) || (map = '=', idx % 1);
+        output += map.charAt(63 & block >> 8 - idx % 1 * 8)
+        ) {
+        charCode = str.charCodeAt(idx += 3/4);
+        if (charCode > 0xFF) {
+            throw new Error("Base64 encoding failed: The string to be encoded contains characters outside of the Latin1 range.");
+        }
+        block = block << 8 | charCode;
+    }
+    return output;
+}
--- typescript/html/DownloadService.ts
+++ typescript/html/DownloadService.ts
@@ -48,7 +48,82 @@ class DownloadService {
             callback(3, error);
         }
 
+    }
 
+//  var url = currentPhotoId;
+//        var xhr = new XMLHttpRequest();
+//        xhr.open('GET', url, true);
+//        xhr.responseType = 'arraybuffer';
+//        xhr.onreadystatechange = function() {
+//            if (xhr.readyState === XMLHttpRequest.DONE) {
+//                if (xhr.status === 200) {
+//                    var response = new Uint8Array(xhr.response);
+//                    var raw = "";
+//                    for (var i = 0; i < response.byteLength; i++) {
+//                        raw += String.fromCharCode(response[i]);
+//                    }
+//
+//                    console.log("image fetched !");
+//
+//                    var image = 'data:image/png;base64,' +Constants.base64Encode(raw);
+//                            //
+//                    img.source = image;
+//                    fetchImages(photoIdUrls);
+//                }
+//            }
+//        }
+//        xhr.send();
+
+
+    public executeBinary(data: DownloadData, callback: Function): void {
+        let httpRequest: XMLHttpRequest = new XMLHttpRequest();
+
+        console.log("trying to fetch data from : " + data.url);
+
+        httpRequest.open('GET', data.url);
+        httpRequest.responseType = 'arraybuffer';
+        // httpRequest.setRequestHeader('Content-Type', data.contentType);
+        if (data.eTag) {
+            httpRequest.setRequestHeader('If-None-Match', data.eTag);
+        }
+        httpRequest.timeout = data.timeout;
+        httpRequest.onreadystatechange = () => {
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                if (httpRequest.status && httpRequest.status == 200 && httpRequest.responseText != "undefined") {
+                    console.log("return status : " + httpRequest.status);
+                    console.log("Resposne Headers : " + httpRequest.getAllResponseHeaders())
+                    console.log("Resposne ETag : " + httpRequest.getResponseHeader("ETag"))
+                    console.log("return responseText : " + httpRequest.responseText.substring(0, 200));
+                    console.log("executing success callback !");
+
+                    var response = new Uint8Array(httpRequest.response);
+                    var raw = "";
+                    for (var i = 0; i < response.byteLength; i++) {
+                        raw += String.fromCharCode(response[i]);
+                    }
+
+                    callback(0, httpRequest, raw);
+
+                    console.log("executing success done !");
+                } else if (httpRequest.status && httpRequest.status === 304) {
+                    callback(1, httpRequest);
+                } else {
+                    console.log("executing failure callback - data : " + httpRequest.response + " " + httpRequest.responseURL + " " + httpRequest.responseText);
+
+                    callback(2, httpRequest);
+                }
+            }
+        };
+        httpRequest.onerror = (ev: Event) => {
+            console.log("error code : " + httpRequest.status);
+            console.log("event : " + ev.currentTarget);
+        }
+
+        try {
+            httpRequest.send()
+        } catch (error) {
+            callback(3, error);
+        }
     }
 
 }
