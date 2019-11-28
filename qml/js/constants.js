44718413b7e5965b0f8bb1d614f17c3a4b8e8543
andywuest <andreas.wuest.freelancer@gmail.com>
2019-11-28 13:32:18 +0100

    updated in/active value

--- qml/js/constants.js
+++ qml/js/constants.js
@@ -1,8 +1,8 @@
 .pragma library
 
 var loggingEnabled = true;
-var CONFERENCE_INACTIVE = 0;
-var CONFERENCE_ACTIVE = 1;
+var CONFERENCE_INACTIVE = 'INACTIVE';
+var CONFERENCE_ACTIVE = 'ACTIVE';
 
 var HTTP_OK = 200;
 var HTTP_NOT_MODIFIED = 304;
@@ -24,20 +24,20 @@ var SINGLE = true;
 //var SINGLE = true;
 
 //FROM https://cdnjs.cloudflare.com/ajax/libs/Base64/1.0.1/base64.js
-function base64Encode (input) {
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-    var str = String(input);
-    for (
-        // initialize result and counter
-        var block, charCode, idx = 0, map = chars, output = '';
-        str.charAt(idx | 0) || (map = '=', idx % 1);
-        output += map.charAt(63 & block >> 8 - idx % 1 * 8)
-        ) {
-        charCode = str.charCodeAt(idx += 3/4);
-        if (charCode > 0xFF) {
-            throw new Error("Base64 encoding failed: The string to be encoded contains characters outside of the Latin1 range.");
-        }
-        block = block << 8 | charCode;
-    }
-    return output;
-}
+//function base64Encode (input) {
+//    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+//    var str = String(input);
+//    for (
+//        // initialize result and counter
+//        var block, charCode, idx = 0, map = chars, output = '';
+//        str.charAt(idx | 0) || (map = '=', idx % 1);
+//        output += map.charAt(63 & block >> 8 - idx % 1 * 8)
+//        ) {
+//        charCode = str.charCodeAt(idx += 3/4);
+//        if (charCode > 0xFF) {
+//            throw new Error("Base64 encoding failed: The string to be encoded contains characters outside of the Latin1 range.");
+//        }
+//        block = block << 8 | charCode;
+//    }
+//    return output;
+//}
